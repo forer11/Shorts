394a8f260565828aebdfc46196b7a6ab0a4e64fd
carmel1996 <carmel.levy@cs.huji.ac.il>
2020-09-23 21:09:44 +0300

    Bug fix-alarm clock is now set without opening the clock app

--- app/src/main/java/com/example/shortmaker/Actions/ActionAlarmClock.java
+++ app/src/main/java/com/example/shortmaker/Actions/ActionAlarmClock.java
@@ -19,8 +19,10 @@ public class ActionAlarmClock implements Action {
         //TODO - we can decide a constant hour to set the alarm clock to
         Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
                 .putExtra(AlarmClock.EXTRA_HOUR,10) //hours in 24 hours format
-                .putExtra(AlarmClock.EXTRA_MINUTES,20);
+                .putExtra(AlarmClock.EXTRA_MINUTES,20)
+                .putExtra(AlarmClock.EXTRA_SKIP_UI, true);
         context.startActivity(intent);
+
     }
 
     @Override
