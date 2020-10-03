201ff6b1a16604564274a1cd854ddbdc96a9ba66
forer11 <forer11@gmail.com>
2020-10-04 00:20:18 +0300

    added toggle for action activation

--- app/src/main/java/com/example/shortmaker/Adapters/ActionAdapter.java
+++ app/src/main/java/com/example/shortmaker/Adapters/ActionAdapter.java
@@ -3,7 +3,9 @@ package com.example.shortmaker.Adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,24 +21,37 @@ import java.util.ArrayList;
 public class ActionAdapter extends RecyclerView.Adapter<ActionAdapter.ActionViewHolder> {
     private ArrayList<ActionData> actionDataList;
     private OnItemLongClickListener longClickListener;
+    private OnSwitchClickListener checkListener;
 
     public interface OnItemLongClickListener {
         void onItemLongClick(View view, int position);
+    }
+
+    public interface OnSwitchClickListener {
+        void onSwitchClick(int position, boolean isChecked);
     }
 
     public void setOnItemLongClickListener(OnItemLongClickListener listener) {
         this.longClickListener = listener;
     }
 
+    public void setOnSwitchClickListener(OnSwitchClickListener listener) {
+        this.checkListener = listener;
+    }
+
 
     public static class ActionViewHolder extends RecyclerView.ViewHolder {
         public ImageView actionIcon;
         public TextView actionTitle;
+        public Switch isEnabledSwitch;
 
-        public ActionViewHolder(View itemView, final OnItemLongClickListener longClickListener) {
+        public ActionViewHolder(View itemView,
+                                final OnItemLongClickListener longClickListener,
+                                final OnSwitchClickListener checkListener) {
             super(itemView);
             actionIcon = itemView.findViewById(R.id.imageView);
             actionTitle = itemView.findViewById(R.id.textView);
+            isEnabledSwitch = itemView.findViewById(R.id.is_enabled_switch);
 
             itemView.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
@@ -45,6 +60,13 @@ public class ActionAdapter extends RecyclerView.Adapter<ActionAdapter.ActionView
                         longClickListener.onItemLongClick(v, getAdapterPosition());
                     }
                     return true;
+                }
+            });
+
+            isEnabledSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    checkListener.onSwitchClick(getAdapterPosition(), isChecked);
                 }
             });
         }
@@ -60,19 +82,15 @@ public class ActionAdapter extends RecyclerView.Adapter<ActionAdapter.ActionView
         View view = LayoutInflater
                 .from(parent.getContext())
                 .inflate(R.layout.action_item_recycler_view, parent, false);
-        return new ActionViewHolder(view, longClickListener);
+        return new ActionViewHolder(view, longClickListener, checkListener);
     }
 
     @Override
     public void onBindViewHolder(@NotNull ActionViewHolder holder, int position) {
         ActionData currentItem = actionDataList.get(position);
-        for (ActionData actionData : ActionFactory.ACTION_DATA_ARRAY_LIST) {
-            if (actionData.getTitle().equals(currentItem.getTitle())) {
-                holder.actionIcon.setImageResource(actionData.getIconPath());
-                holder.actionTitle.setText(actionData.getTitle());
-                break;
-            }
-        }
+        holder.actionIcon.setImageResource(currentItem.getIconPath());
+        holder.actionTitle.setText(currentItem.getTitle());
+        holder.isEnabledSwitch.setChecked(currentItem.getIsActivated());
 
     }
 
