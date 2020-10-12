3407060f6876518de1ad90c577f0f5169b62bb90
carmel1996 <carmel.levy@cs.huji.ac.il>
2020-10-12 13:34:26 +0300

    added buttons in flash dialog

--- app/src/main/java/com/shorts/shortmaker/ActionDialogs/FlashActionDialog.java
+++ app/src/main/java/com/shorts/shortmaker/ActionDialogs/FlashActionDialog.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +19,8 @@ import java.util.ArrayList;
 
 public class FlashActionDialog extends ActionDialog {
 
+    private Button okButton;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -26,32 +29,21 @@ public class FlashActionDialog extends ActionDialog {
         View view = layoutInflater.inflate(R.layout.flash_dialog, null);
 
         initializeDialogViews(view);
-        buildDialog(builder, view);
+        buildDialog(builder, view, "Turn on the Flashlight", okButton);
         return builder.create();
     }
 
     protected void initializeDialogViews(View view) {
         ImageView imageView = view.findViewById(R.id.imageView);
         setDialogImage(imageView, R.drawable.flash_gif);
+        okButton = view.findViewById(R.id.okButton);
     }
 
-    protected void buildDialog(AlertDialog.Builder builder, View view) {
-        builder.setView(view)
-                .setTitle("Turn on the Flashlight")
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                })
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ArrayList<String> results = new ArrayList<>();
-                        results.add("0");
-                        String description = "Flashlight On";
-                        listener.applyUserInfo(results, description);
-                    }
-                });
+    protected void getUserInput() {
+        ArrayList<String> results = new ArrayList<>();
+        results.add("0");
+        String description = "Flashlight On";
+        listener.applyUserInfo(results, description);
     }
 }
 
